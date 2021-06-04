931dcf59aa4c8a386d9a58f15fbc13e5ee4b0981
LiubovShpakova <68159626+LiubovShpakova@users.noreply.github.com>
2021-06-04 11:02:31 +0300

    add check step 2.1

--- contacts.js
+++ contacts.js
@@ -1,28 +1,31 @@
-// import * as fs from "fs/promises";
-// import * as path from "path";
-
 const fs = require('fs').promises;
 const path = require('path');
 const contactsPath = path.join('db', 'contacts.json');
 
-function listContacts() {
-  // ...твой код
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath);
+    const result = JSON.parse(data);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function getContactById(contactId) {
-  // ...твой код
-}
+// function getContactById(contactId) {
+//   // ...твой код
+// }
 
-function removeContact(contactId) {
-  // ...твой код
-}
+// function removeContact(contactId) {
+//   // ...твой код
+// }
 
-function addContact(name, email, phone) {
-  // ...твой код
-}
+// function addContact(name, email, phone) {
+//   // ...твой код
+// }
 module.exports = {
   listContacts,
-  getContactById,
-  removeContact,
-  addContact,
+  // getContactById,
+  // removeContact,
+  // addContact,
 };
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log(123123);
+const argv = require('yargs').argv;
+// TODO: рефакторить
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      console.log('list');
+      break;
+    case 'get':
+      console.log('id', id);
+      break;
+    case 'add':
+      console.log('name email phone', name, email, phone);
+      break;
+    case 'remove':
+      console.log('id', id);
+      break;
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+invokeAction(argv);

0bda8834ef44dc1ed838b263e44c11598a086381
LiubovShpakova <68159626+LiubovShpakova@users.noreply.github.com>
2021-07-10 00:37:27 +0300

    hw-06-v1

---
+++ services/email.js
@@ -0,0 +1,65 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable require-jsdoc */
+const Mailgen = require('mailgen');
+require('dotenv').config();
+
+
+class EmailService {
+  constructor(env, sender) {
+    this.sender = sender;
+    switch (env) {
+      case 'development':
+        this.link = 'http://localhost:3000';
+        break;
+      case 'production':
+        this.link = 'link for production';
+        break;
+      default:
+        this.link = 'http://localhost:3000';
+        break;
+    }
+  };
+
+  createTemplateVerifyEmail(verifyToken) {
+    const mailGenerator = new Mailgen({
+      theme: 'salted',
+      product: {
+        name: 'LSCompany',
+        link: this.link,
+        // Optional product logo
+        // logo: '../public/logo/shortcut.png',
+      },
+    });
+    const email = {
+      body: {
+        intro:
+          'Welcome to LSCompany! We\'re very excited to have you on board.',
+        action: {
+          instructions: 'To get started with LSCompany, please click here:',
+          button: {
+            color: '#29bac2', // Optional action button color
+            text: 'Verify your account',
+            link: `${this.link}/api/users/verify/${verifyToken}`,
+          },
+        },
+      },
+    };
+
+    const emailBody = mailGenerator.generate(email);
+    return emailBody;
+  };
+
+  async sendVerifyEmail(verifyToken, email) {
+    const emailHtml = this.createTemplateVerifyEmail(verifyToken);
+    const msg = {
+      to: email, // Change to your recipient
+      subject: 'Verify email',
+      html: emailHtml,
+    };
+    const result = await this.sender.send(msg);
+    console.log('EmailService -> sendVerifyEmail -> result', result);
+  }
+};
+
+module.exports = EmailService;
+
